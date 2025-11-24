98f00832fb405c0c9f13bd70b3a56300309352f8
Lily Kiziriya <107429746+LilyKizir@users.noreply.github.com>
2025-11-23 19:58:35 -0500

    Created Vite Project with Bootstrap

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+function App() {
+
+  return (
+    <>
+      <p>This is app</p>
+    </>
+  )
+}
+
+export default App
